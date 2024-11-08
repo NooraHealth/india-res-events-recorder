4de302b22ce066cf502e80d2487451a47773f957
Sreeram <sreeramramasubramanian@NH-PLC-LT-0004.local>
2024-11-08 13:16:54 +0530

    implemented patient care confirmation and hcw care confirmation. Testing in progress

--- app/services/alerts/hcw_confirm_care.rb
+++ app/services/alerts/hcw_confirm_care.rb
@@ -1,17 +1,25 @@
+# this class will accept responses from healthcare workers for a particular patient
+# and record them as responses in the health_alert_responses table
+# This will come from within the journey on Turn
+
 require_relative '../../../lib/utils.rb'
 require_relative '../../exceptions/http.rb'
 
 module Alerts
   class HcwConfirmCare < MessageEvents::Base
 
-    def initialize(logger, hcw_type, phone, patient_phone)
-
+    def initialize(logger, turn_params)
       super(logger)
+      @hcw_type = turn_params[:hcw_type]
+      @phone = turn_params[:phone]
+      @patient_phone = turn_params[:patient_phone]
+    end
 
-      if hcw_type == "AnmUser"
-        hcw_user = AnmUser.find_by_phone(phone)
-      elsif hcw_type == "AshaUser"
-        hcw_user = AshaUser.find_by_phone(phone)
+    def call
+      if @hcw_type == "AnmUser"
+        hcw_user = AnmUser.find_by_phone(@phone)
+      elsif @hcw_type == "AshaUser"
+        hcw_user = AshaUser.find_by_phone(@phone)
       else
         raise BadRequest.new("hcw_type can be one of AnmUser/AshaUser")
       end
@@ -21,24 +29,29 @@ module Alerts
         raise Forbidden.new("Hcw doesn't qualify for confirming care")
       end
 
-      @patient_user = User.find_by_phone(phone)
+      @patient_user = User.find_by_phone(@patient_phone)
       unless user_ids.include? @patient_user.id
         raise Forbidden.new("Asha is not allowed to confirm care for this user")
       end
 
       notification = hcw_user.get_most_recent_health_alert_notification(@patient_user.id)
 
-      byebug
+      if notification.blank?
+        self.errors << "No health alert notification found for user with mobile number: #{@patient_phone}"
+        return self
+      end
 
-    end
+      # create a health_alert_response object for this notification
+      response = HealthAlertResponse.new(user: @patient_user,
+                                         health_alert_id: notification.health_alert_id,
+                                         health_alert_notification_id: notification.id,
+                                         platform: "turn")
+      unless response.save
+        self.errors << "Could not save health alert response because: #{response.errors.full_messages}"
+        return self
+      end
 
-
-    def confirm
-      # TODO:
-    end
-
-    def deny
-      # TODO:
+      self
     end
 
   end
--- app/services/alerts/patient_confirm_care.rb
+++ app/services/alerts/patient_confirm_care.rb
@@ -16,8 +16,36 @@ module Alerts
     end
 
     def call
-      
 
+      mobile_number = extract_mobile_number("textit", textit_params[:urns])
+      # look for user and find their latest health_alert_notification
+      @res_user = User.find_by mobile_number: mobile_number
+
+      if @res_user.blank?
+        self.errors << "User not found in database with mobile number: #{mobile_number}"
+        return self
+      end
+
+      # find the latest health alert notification for this user
+      notification = @res_user.get_most_recent_health_alert_notification
+      if notification.blank?
+        self.errors << "No health alert notification found for user with mobile number: #{mobile_number}"
+        return self
+      end
+
+      # create a health_alert_response object for this notification
+      response = HealthAlertResponse.new(user: @res_user,
+                                         health_alert_id: notification.health_alert_id,
+                                         health_alert_notification_id: notification.id,
+                                         platform: "textit")
+      unless response.save
+        self.errors << "Could not save health alert response because: #{response.errors.full_messages}"
+        return self
+      end
+
+      # TODO - create event to signify health alert being resolved
+
+      self
     end
   end
 end
